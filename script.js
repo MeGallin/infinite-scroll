fcabe2d55f78c99ae0a58ae1fc0a255b5754ba07
Guy <me@garyallin.uk>
2020-05-06 15:08:25 +0100

    forking: fetch API

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const postsContainer = document.getElementById('post-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
 
+let limit = 3;
+let page = 1;
+
+// Fetch from API
+let getPosts = async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+// Show all post in DOM
+let showPost = async () => {
+  const post = await getPosts();
+  console.log(post);
+
+  post.forEach((post) => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+      `;
+    postsContainer.appendChild(postEl);
+  });
+};
+
+// show posts
+showPost();
